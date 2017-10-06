9373535ab338c70fb0a50ca05be5727dd6b7d972
Arthur Geron <arthurgeron@hotmail.com>
2017-10-06 02:52:20 -0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,4 @@
+# Insta Note Extension
+
+Simple extension made for instantly sharing text between your phone and browser.
+Coming soon to Android, Firefox, Opera and Chrome.
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Loginscreen from './Loginscreen'
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen parentContext={this}/>);
+    this.setState({
+      loginPage:loginPage
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Insta Note</h1>
+        </header>
+        <div className="App">
+          {this.state.loginPage}
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
