031aefaa840949d410e253e2c29d04be8ca20dee
Arthur Geron <arthurgeron@hotmail.com>
2017-10-10 16:44:26 -0300

    Separated code for reac-native/mobile only.

--- InstaNoteMobile/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-global.isNative = true;
-AppRegistry.registerComponent('InstaNoteMobile', () => App);
--- src/Login.test.js
+++
@@ -1,8 +0,0 @@
-import Login from "./Login";
-import ReactDOM from "react-dom";
-import React from "react";
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Login />, div);
-});
