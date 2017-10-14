c7ed3de5a0c9af52240a6e62e3f3267f846bcfaf
Arthur Geron <arthurgeron@hotmail.com>
2017-10-14 16:14:50 -0300

    Set animation to splash screen

--- Splash.js
+++ Splash.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Image, Animated, Easing } from 'react-native';
 class Splash extends Component {
   constructor (props) {
     super(props);
+    this.animationValue = new Animated.Value(0);
   }
   componentWillMount () {
     let self = this;
     setTimeout (() => {
-      self.props.parentContext.setState({splash: false});
+      //self.props.parentContext.setState({splash: false});
     }, 2000);
   }
+  rotateImage() {
+    this.animationValue.setValue(0);
+    Animated.timing(
+      this.animationValue,
+      {
+        toValue: 1,
+        duration:3000,
+        easing: Easing.linear
+      }
+    ).start(() => this.rotateImage())
+  }
   render () {
+    const spin = this.animationValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
     return (
       <View>
-        <Image style={{
+        <Animated.Image style={{
           left: '25%',
           top: '80%',
           alignContent: 'center',
-          justifyContent: 'center'
+          justifyContent: 'center',
+          transform: [{rotate: spin}]
         }} source={require('./android/app/src/main/res/mipmap-xxxhdpi/instanote.png')}/>
       </View>
     );
